1922badaaae291862aab10e02b254f8220f789e9
David Hilscher <dhilscher@Davids-MacBook-Air.local>
2017-09-06 15:32:19 -0700

    photograph post types/taxonomy added

--- plugins/danielfox-functionality/lib/functions/post-types.php
+++ plugins/danielfox-functionality/lib/functions/post-types.php
@@ -6,3 +6,61 @@
  */
 
 // Add your custom post types here...
+
+function danielfox_cpt_photograph() {
+
+	$labels = array(
+		'name'                  => 'Photographs',
+		'singular_name'         => 'Photograph',
+		'menu_name'             => 'Photographs',
+		'name_admin_bar'        => 'Photograph',
+		'archives'              => 'Photograph Archives',
+		'attributes'            => 'Photograph Attributes',
+		'parent_item_colon'     => 'Parent Photograph:',
+		'all_items'             => 'All Photographs',
+		'add_new_item'          => 'Add New Photograph',
+		'add_new'               => 'Add New',
+		'new_item'              => 'New Photograph',
+		'edit_item'             => 'Edit Photograph',
+		'update_item'           => 'Update Photograph',
+		'view_item'             => 'View Photograph',
+		'view_items'            => 'View Photographs',
+		'search_items'          => 'Search Photograph',
+		'not_found'             => 'Not found',
+		'not_found_in_trash'    => 'Not found in Trash',
+		'featured_image'        => 'Featured Image',
+		'set_featured_image'    => 'Set featured image',
+		'remove_featured_image' => 'Remove featured image',
+		'use_featured_image'    => 'Use as featured image',
+		'insert_into_item'      => 'Insert into Adventure',
+		'uploaded_to_this_item' => 'Uploaded to this Photograph',
+		'items_list'            => 'Photographs list',
+		'items_list_navigation' => 'Photographs list navigation',
+		'filter_items_list'     => 'Filter Photographs list',
+	);
+	$args = array(
+		'label'                 => 'Photograph',
+		'description'           => 'Custom Photograph Post Type',
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'author', 'thumbnail', 'revisions', ),
+		'hierarchical'          => true,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'menu_icon'             => 'dashicons-location-alt',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => 'photographs',		
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'post',
+		'show_in_rest'          => true,
+	);
+	register_post_type( 'photograph', $args );
+
+    //Clear Permalinks
+    flush_rewrite_rules();
+}
+add_action( 'init', 'danielfox_cpt_photograph', 0 );
--- plugins/danielfox-functionality/lib/functions/taxonomies.php
+++ plugins/danielfox-functionality/lib/functions/taxonomies.php
@@ -6,3 +6,40 @@
  */
 
 // Add your custom taxonomies here...
+
+function danielfox_tax_album_type() {
+	$labels = array(
+		'name'                       => 'Album Types',
+		'singular_name'              => 'Album Type',
+		'menu_name'                  => 'Album Types',
+		'all_items'                  => 'All Album Types',
+		'parent_item'                => 'Parent Album Type',
+		'parent_item_colon'          => 'Parent Album Type:',
+		'new_item_name'              => 'New Album Type Name',
+		'add_new_item'               => 'Add New Album Type',
+		'edit_item'                  => 'Edit Album Type',
+		'update_item'                => 'Update Album Type',
+		'view_item'                  => 'View Album Type',
+		'separate_items_with_commas' => 'Separate items with commas',
+		'add_or_remove_items'        => 'Add or remove items',
+		'choose_from_most_used'      => 'Choose from the most used',
+		'popular_items'              => 'Popular Album Types',
+		'search_items'               => 'Search Album Types',
+		'not_found'                  => 'Not Found',
+		'no_terms'                   => 'No Album Types',
+		'items_list'                 => 'Album Types list',
+		'items_list_navigation'      => 'Album Types list navigation',
+	);
+	$args = array(
+		'labels'                     => $labels,
+		'hierarchical'               => true,
+		'public'                     => true,
+		'show_ui'                    => true,
+		'show_admin_column'          => true,
+		'show_in_nav_menus'          => true,
+		'show_tagcloud'              => true,
+		'show_in_rest'               => true,
+	);
+	register_taxonomy( 'album-type', array( 'album' ), $args );
+}
+add_action( 'init', 'danielfox_tax_album_type', 0 );
