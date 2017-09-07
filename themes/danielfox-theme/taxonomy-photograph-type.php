b135547a1e6fb7d1af45e5d15fbca4a6b8ef4f34
David Hilscher <dhilscher@Davids-MacBook-Air.local>
2017-09-07 10:36:34 -0700

    added taxonomy-photograph-type / fixed mixins error

---
+++ themes/danielfox-theme/taxonomy-photograph-type.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * The template for displaying archive pages.
+ *
+ * @package Daniel_Fox_Theme
+ */?>
+
+<?php get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<?php if ( have_posts() ) : ?>
+
+			<header class="page-header">
+				<?php
+					the_archive_title( '<h1 class="page-title">', '</h1>' );
+					the_archive_description( '<div class="taxonomy-description">', '</div>' );
+				
+?>
+			</header><!-- .page-header -->
+
+			<?php /* Start the Loop */ ?>
+<section class="photograph-grid">
+<?php while ( have_posts() ) : the_post(); ?>
+		<div class="photograph-grid-item" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			<header class="entry-header">
+				<div class="thumbnail-wrapper">
+				<?php if ( has_post_thumbnail() ) : ?>
+					<a href="<?php the_permalink(); ?>">
+					<?php the_post_thumbnail( 'medium' ); ?>
+					</a>
+				<?php endif; ?>
+				</div>
+				<div class="photograph-info">
+					<?php the_title( sprintf( '<h2 class="entry-title">', esc_url( get_permalink() ) ), '.....', CFS()->get( 'price' ), '</h2>' ); ?>
+					<?php echo CFS()->get( 'price' ); ?>
+				</div>
+			</header><!-- .entry-header -->
+		</div>
+<?php endwhile; ?>
+</section>
+	<?php else : ?>
+		<?php get_template_part( 'template-parts/content', 'none' ); ?>
+	<?php endif; ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
+
+
