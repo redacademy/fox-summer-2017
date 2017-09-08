9b80c30aae052305fc2973b5264289a1122fc893
DanielKell <danielkell.ubc@gmail.com>
2017-09-08 14:21:57 -0700

    Header nav bar toggles now on media breakpoint at 600px

--- themes/danielfox-theme/js/menu-toggle.js
+++ themes/danielfox-theme/js/menu-toggle.js
@@ -3,6 +3,7 @@ $( document ).ready(function() {
 $('.nav-side .nav-toggle').on('click', function(e) {
   e.preventDefault();
   $(this).parent().toggleClass('nav-open');
+  $('.header-nav-bar').css('top', '-100px');
 
   //Write instructions here to change CSS for .header-nav-bar AND hide header-page-name AND hide header-logo-mobile
 });
@@ -10,6 +11,7 @@ $('.nav-side .nav-toggle').on('click', function(e) {
     $(document).click( function(event){
       if( !$('.nav-side .nav-open, .nav-side .nav-toggle').is(event.target)){
       $('.nav-side').removeClass('nav-open');
+      $('.header-nav-bar').css('top', '0px');
       }
     });
 
