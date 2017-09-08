c690cab55e53785ebe267ca9a836643a42c533aa
David Hilscher <dhilscher@Davids-MacBook-Air.local>
2017-09-08 14:59:29 -0700

    subscribe toggle javascript

--- themes/danielfox-theme/footer.php
+++ themes/danielfox-theme/footer.php
@@ -13,12 +13,13 @@
 				<div class="footer-left">
 					<img src="<?php echo get_template_directory_uri(); ?>/assets/Facebook_White.svg" alt="Facebook logo" />
 					<img src="<?php echo get_template_directory_uri(); ?>/assets/Instagram_White.svg" alt="Instagram logo" />
-					<div class="subscribe-container">
-						<form method="POST" action="<?php echo get_template_directory_uri(); ?>/subscribe.php">
-						<input class="subscribe-button" type="submit" value="Subscribe" name="Subscribe">
-						<input class="subscribe-email-input" type="text" name="Email" placeholder="Email" size="20">
+					<!--<div class="subscribe-container">
+						<form method="POST" action=" ">
+							<p><a href=" " class="search-toggle" aria-hidden="true">Subscribe</a></p>
+							<input class="subscribe-button" type="submit" value="Subscribe" name="Subscribe">
+							<input class="subscribe-email-input" type="text" name="Email" placeholder="Email" size="20">
 						</form>
-					</div>
+					</div>-->
 				</div>
 				<div class="footer-right">
 
