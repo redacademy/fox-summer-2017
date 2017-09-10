408f453ab50418b27fca8502adaaeef604457f75
DanielKell <danielkell.ubc@gmail.com>
2017-09-10 16:19:14 -0700

    Added placeholder videos to speaker page

--- themes/danielfox-theme/speaker.php
+++ themes/danielfox-theme/speaker.php
@@ -10,19 +10,37 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
-		        <div class="speaker-header-image">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/Speaker1.jpg" alt="Daniel fox speaking" />
-                </div>
-                <section class="front-page-photography">
-                <div class="front-page-header-box">
-                <h2>What I Speak About</h2>
-                </div>
-                <p>Through my speaking engagements, I share what personal insights and realizations I’ve collected from my own experiences, and through my interactions with the natural world.
-				<br></br>I strive for us to connect more  with nature and with one another. 
-				<br></br>In a world so fast-paced and filled with endless distractions, it’s easy to get caught up in a more materialistic way of thinking. I encourage myself and the peers I share with to be aware of what’s truly valuable in life, and to use our connection with nature as a constant reminder to exist in the moment. 
-                </p>
-
+			<div class="speaker-top-half">
+				<div class="speaker-header-image">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/images/Speaker1.jpg" alt="Daniel fox speaking" />
+				</div>
+				<section class="speaker-description">
+					<div class="front-page-header-box speaker-header-box">
+						<h2>What I Speak About</h2>
+					</div>
+					<p>Through my speaking engagements, I share what personal insights and realizations I’ve collected from my own experiences, and through my interactions with the natural world.
+					<br></br>I strive for us to connect more with nature and with one another. 
+					<br></br>In a world so fast-paced and filled with endless distractions, it’s easy to get caught up in a more materialistic way of thinking. I encourage myself and the peers I share with to be aware of what’s truly valuable in life, and to use our connection with nature as a constant reminder to exist in the moment. 
+					</p>
+				</section>
+			</div>
+			<div class="speaker-bottom-half">
+				<div class="speaker-video-one">
+					<iframe width="600" height="450"
+					src="https://www.youtube.com/embed/yhUCG-9s8kA">
+					</iframe>
+				</div>
+				<div class="speaker-video-two">
+					<iframe width="600" height="450"
+					src="https://www.youtube.com/embed/ArhjLa4xbNk">
+					</iframe>
+				</div>
+				<div class="speaker-video-three">
+					<iframe width="600" height="450"
+					src="https://www.youtube.com/embed/Dhas9OEc1Lk">
+					</iframe>
+				</div>
+			</div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
