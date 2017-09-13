30147f679c47c97a3c08756a977caa38e45e7abd
David Hilscher <davidhilscher@gmail.com>
2017-09-13 13:38:10 -0700

    contact form widget area created

--- themes/danielfox-theme/header.php
+++ themes/danielfox-theme/header.php
@@ -55,7 +55,6 @@
 					</div>
 					
 					<div class="nav-menu-options">
-					<!--<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?></button>-->
 					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 					</div>
 
