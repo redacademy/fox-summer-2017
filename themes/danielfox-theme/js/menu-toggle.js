db24433fcaecf3f732702dfd2ed08ce6b8e7995d
DanielKell <danielkell.ubc@gmail.com>
2017-09-12 21:02:04 -0700

    Fixed sidebar to close properly when clicked outside of

--- themes/danielfox-theme/js/menu-toggle.js
+++ themes/danielfox-theme/js/menu-toggle.js
@@ -1,18 +1,23 @@
 (function ($) {
 $( document ).ready(function() {
+
 $('.nav-side .nav-toggle').on('click', function(e) {
   e.preventDefault();
   $(this).parent().toggleClass('nav-open');
   $('.header-nav-bar').css('top', '-100px');
 
+  $('.site-content').click( function(){
+    $('.nav-side').removeClass('nav-open');
+    $('.header-nav-bar').css('top', '0px');
+  }); 
 });
 
-    $(document).click( function(event){
-      if( !$('.nav-side .nav-open, .nav-side .nav-toggle').is(event.target)){
-      $('.nav-side').removeClass('nav-open');
-      $('.header-nav-bar').css('top', '0px');
-      }
-    });
+// Sliding out more options for Photography in mobile sidebar
+$('#menu-item-98').on('click', function(e) {
+  e.preventDefault();
+  $('.site-header .header-nav-bar .burger-nav .menu .sub-menu').toggleClass('sub-menu--open');
+});
+
 
 });
 }(jQuery));
