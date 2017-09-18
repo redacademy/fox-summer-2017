59813d61ede2d8477d7dd4866f37543982a8d768
DanielKell <danielkell.ubc@gmail.com>
2017-09-18 10:37:00 -0700

    Fixed bugs: Mobile modal window now correctly opens and closes for all posts. Dates now display correctly on desktop modal window 2

--- themes/danielfox-theme/js/menu-toggle.js
+++ themes/danielfox-theme/js/menu-toggle.js
@@ -1,30 +1,41 @@
 (function ($) {
-$( document ).ready(function() {
-
-$('.nav-side .nav-toggle').on('click', function(e) {
-  e.preventDefault();
-  $(this).parent().toggleClass('nav-open');
-  $('.header-nav-bar').css('top', '-100px');
-  $('.nav-toggle').hide();
-
-  $('.site-content').click( function(){
-    $('.nav-side').removeClass('nav-open');
-    $('.header-nav-bar').css('top', '0px');
-    $('.nav-toggle').show();
-  }); 
-
-});
-
-  $('.mobile-modal-description-toggle').on('click', function() {
-   $('#mobile-modal-details').toggleClass('mobile-modal-description-window');
-});
+  $( document ).ready(function() {
 
 
-// Sliding out more options for Photography in mobile sidebar
-$('#menu-item-98 a').on('click', function() {
-  $('.site-header .header-nav-bar .burger-nav .menu .sub-menu').toggleClass('sub-menu--open');
-});
+    var $mobileDetails =  $('.mobile-modal-details');
+
+    $('.nav-side .nav-toggle').on('click', function(e) {
+      e.preventDefault();
+      $(this).parent().toggleClass('nav-open');
+      $('.header-nav-bar').css('top', '-100px');
+      $('.nav-toggle').hide();
+
+      $('.site-content').click( function(){
+        $('.nav-side').removeClass('nav-open');
+        $('.header-nav-bar').css('top', '0px');
+        $('.nav-toggle').show();
+      }); 
+
+      
+
+    });
 
 
-});
+    $('.gallery-modal-link').on('click', function(){
+      $mobileDetails.removeClass('mobile-modal-description-window');
+    });
+
+    // toggle description in modal
+    $('.mobile-modal-description-toggle').on('click', function() {
+      $mobileDetails.toggleClass('mobile-modal-description-window');
+    });
+
+
+    // Sliding out more options for Photography in mobile sidebar
+    $('#menu-item-98 a').on('click', function() {
+     $('.site-header .header-nav-bar .burger-nav .menu .sub-menu').toggleClass('sub-menu--open');
+    });
+
+
+  });
 }(jQuery));
