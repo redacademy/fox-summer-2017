0715bba6c5b3f40f5425f6204821153851392fd7
DanielKell <danielkell.ubc@gmail.com>
2017-09-19 10:28:06 -0700

    Added CFS loading to speaker page videos. Minor styling

--- themes/danielfox-theme/single-project.php
+++ themes/danielfox-theme/single-project.php
@@ -19,7 +19,8 @@ get_header(); ?>
 			</div>
 
 			<?php if ( has_post_thumbnail() ) : ?>
-				<div class="featured-image"><?php the_post_thumbnail( 'large' ); ?></div>
+				<div class="featured-image project-featured-image" ><?php the_post_thumbnail( 'large' ); ?></div>
+				
 			<?php endif; ?>
 
 			<div class="entry-content">
